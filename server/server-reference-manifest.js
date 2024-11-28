fe93691dfffee3475ebcd5c3a00e13abb9cef755
cafe1004 <cafe1004@gmail.com>
2024-11-28 20:31:45 +0900

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"oyFu1SjFP5WL0zPg1CDJReirDcbRKWfgfBPmEFnfpuM=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"9Xt/g7aghGOlKAM6AbpIJWS+Ua7RB6HVj+iGuBJoEKI=\"}"
